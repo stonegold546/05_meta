bbfd8d5acaa05c5af92f26fd3da36e4ad0e47d07
James <stonegold546@gmail.com>
2017-06-07 19:00:06 -0400

    My work

---
+++ source_code.R
@@ -0,0 +1,68 @@
+repo <- "https://cloud.r-project.org"
+
+# How to use the MAVIS package:
+if (!require(MAVIS)) {
+  install.packages("MAVIS", repos = repo)
+  library(MAVIS)
+}
+startmavis()
+
+# Load/install the packages we require
+# I'm loading lattice simply to obtain plots to visualize the data.
+sapply(c("metafor", "lattice"), function(package_name) {
+  if (!require(package_name, character.only = TRUE)) {
+    install.packages(package_name, repos = repo)
+    library(package_name, character.only = TRUE)
+  }
+})
+
+# Load the dataset we need
+data <- dat.bangertdrowns2004
+head(data)
+# Run ?dat.bangertdrowns2004 to see what the dataset represents
+
+# Plotting our data
+xyplot(
+  x ~ Group.1, aggregate(
+    as.numeric(data$id), by = list(data$year), FUN = length
+  ), type = "o", main = "Effect sizes over time", grid = TRUE,
+  xlab = "Year", ylab = "Number of effect sizes"
+)
+histogram(~ grade, data, main = "Effect size distribution by grade")
+histogram(
+  ~ wic + feedback + info + pers + imag + meta,
+  main = "Proportions of possible moderators",
+  scales = list(x = list(at = c(0, 1), labels = c("no", "yes"))),
+  data, layout = c(3, 2)
+)
+densityplot(~ length, data, plot.points = FALSE, auto.key = TRUE)
+densityplot(~ minutes, data, plot.points = FALSE)
+densityplot(~ yi, data, xlab = "Standardized mean difference")
+densityplot(~ ni, data, xlab = "Sample sizes")
+
+# Conduct meta-analysis
+(res.0 <- rma(
+  yi = yi, vi = vi, ni = ni, data = data,
+  measure = "SMD", test = "knha"
+))
+confint(res.0, level = .95) # Obtain CI on heterogeneity estimates
+
+# Forest plot
+forest(
+  res.0, slab = paste0(data$author, " (", data$year, ")"),
+  xlim = c(-2.5, 2.5), cex = .75
+)
+op <- par(cex = 1, font = 1)
+text(-2.5, 50, "Author(s) (Year)", pos = 4)
+text(2.9775, 50, "SMD [95% CI]", pos = 2)
+
+# Moderation analysis with categorical variable
+(res.1 <- rma(
+  yi = yi, vi = vi, ni = ni, data = data,
+  mods = ~ factor(grade),
+  measure = "SMD", test = "knha"
+))
+
+# Publication bias
+funnel(res.0) # Funnel plot
+regtest(res.0) # Test of asymmetry
